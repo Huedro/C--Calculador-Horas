f99d705651f9af316494ed6f45e448b9e8d4f0d2
HPedro <59668414+Huedro@users.noreply.github.com>
2021-10-22 15:09:03 -0300

    Commit inicial

---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+
+            string[] vet = Console.ReadLine().Split(' ');
+            int initialHour = int.Parse(vet[0]);
+            int finalHour = int.Parse(vet[1]);
+
+            int duration;
+
+            if (initialHour < finalHour)
+            {
+                duration = finalHour - initialHour;
+            }
+            else
+            {
+                duration = 24 - initialHour + finalHour;
+            }
+
+            Console.WriteLine("O JOGO DUROU {0} HORA(S)", duration);
+
+        }
+    }
+}
